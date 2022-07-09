14a28cc76776178275e4a25e83a975be2405df74
Ahmad-khatib <ahmadkhatib923@gmail.com>
2022-07-09 20:06:14 +0300

    firendsSidebar continued

--- discord/react-client/src/Dashboard/FriendsSidebar/FriendsList/FriendsList.js
+++ discord/react-client/src/Dashboard/FriendsSidebar/FriendsList/FriendsList.js
@@ -1,5 +1,13 @@
 import React from "react";
 import { styled } from "@mui/system";
+import FriendsListItem from "./FriendsListItem";
+
+const dummy = [
+  { id: 1, username: "ahmad", isOnline: true },
+  { id: 2, username: "ali", isOnline: false },
+  { id: 3, username: "noor", isOnline: true },
+  { id: 4, username: "reem", isOnline: false },
+];
 
 const MainContainer = styled("div")({
   flexGrow: 1,
@@ -7,7 +15,18 @@ const MainContainer = styled("div")({
 });
 
 const FriendsList = () => {
-  return <MainContainer></MainContainer>;
+  return (
+    <MainContainer>
+      {dummy.map((friend) => (
+        <FriendsListItem
+          username={friend.username}
+          id={friend.id}
+          isOnline={friend.isOnline}
+          key={friend.id}
+        />
+      ))}
+    </MainContainer>
+  );
 };
 
 export default FriendsList;
---
+++ discord/react-client/src/Dashboard/FriendsSidebar/PendingInvitationsList/PendingInvitationsListItem.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Box, Tooltip, Typography } from "@mui/material";
+import Avatar from "../../../shared/components/Avatar";
+import InvitationDecisionButtons from "./InvitationDecisionButtons";
+
+const PendingInvitationsListItem = ({
+  key,
+  id,
+  username,
+  mail,
+  acceptFriendInvitation = () => {},
+  rejectFriendInvitation = () => {},
+}) => {
+  const [buttonsDisabled, setButtonDisabled] = useState(false);
+  const handleAcceptInvitation = () => {
+    acceptFriendInvitation({ id });
+    setButtonDisabled(true);
+  };
+  const handleRejectInvitation = () => {
+    rejectFriendInvitation({ id });
+    setButtonDisabled(true);
+  };
+
+  return (
+    <Tooltip title={mail}>
+      <div style={{ width: "100%" }}>
+        <Box
+          sx={{
+            width: "100%",
+            height: "42px",
+            marginTop: "7px",
+            display: "flex",
+            alignItems: "center",
+            justifyCOntent: "space-between",
+          }}>
+          <Avatar username={username}> </Avatar>
+          <Typography
+            sx={{
+              marginLeft: "7px",
+              fontWeight: "700",
+              color: "#8e9297",
+              flexGrow: 1,
+            }}
+            variant="subtitle1">
+            {username}{" "}
+          </Typography>
+          <InvitationDecisionButtons
+            disabled={buttonsDisabled}
+            acceptInvitationHandler={handleAcceptInvitation}
+            rejectInvitationHandler={handleRejectInvitation}
+          />
+        </Box>
+      </div>
+    </Tooltip>
+  );
+};
+
+export default PendingInvitationsListItem;
